a02587f5099f4465fc00010076601a31be5f2c1f
Tian Le Hou <tianlehou1983@gmail.com>
2025-06-19 22:09:26 -0500

    2025-06-19 v1.02.004: users/candidate - cambios de iconos mas acordes a las secciones de Perfil, Suscripciones y Referir del componente sidebar. - en el componente home se ajusto el header en posición sticky y dirige a las otras secciones al hacer click en los links. se cambiaron los iconos de Font awesome, por iconos de ionic.

--- src/app/pages/users/candidate/components/sidebar/home/home.component.ts
+++ src/app/pages/users/candidate/components/sidebar/home/home.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  scrollTo(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
